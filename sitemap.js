06116a969004aa66cefec04c7b9b0b5fe6944ae8
Riaz Hasan <riaz.hasan@merkle.com>
2023-05-04 10:22:02 +1000

    Additional page types added

--- sitemap.js
+++ sitemap.js
@@ -22,8 +22,65 @@ SalesforceInteractions.init({
 
         pageTypes: [
             {
-                name: "View Homepage"
+                name: "home",
+                isMatch: () => (/^\/$/).test(window.location.pathname),
+                interaction: {name: "View Homepage"}
+            },
+            {
+                name: "specsavers",
+                isMatch: () => (/\/perks\/specsavers/).test(window.location.pathname),
+                interaction: {name: "View Specsavers"}
+            },
+            {
+                name: "goodlife",
+                isMatch: () => (/\/perks\/goodlife/).test(window.location.pathname),
+                interaction: {name: "View Goodlife Gyms"}
+            },
+            {
+                name: "perks",
+                isMatch: () => (/^\/perks$/).test(window.location.pathname),
+                interaction: {name: "View perks"}
+            },
+            
+            {
+                name: "phi",
+                isMatch: () => (/\/starter-silver$/).test(window.location.pathname),
+                interaction: {name: "View Item Hospital"}
+            },
+            {
+                name: "phi",
+                isMatch: () => (/\/extras-cover\/black-70-extras/).test(window.location.pathname),
+                interaction: {name: "View Item Extras"}
+            },
+            {
+                name: "category",
+                isMatch: () => (/\/health-insurance$/).test(window.location.pathname),
+                interaction: {name: "View Health Insurance"}
             }
+            ,
+            {
+                name: "category",
+                isMatch: () => (/\/travel-insurance$/).test(window.location.pathname),
+                interaction: {name: "View Travel Insurance"}
+            },
+            {
+                name: "category",
+                isMatch: () => (/\/car-insurance$/).test(window.location.pathname),
+                interaction: {name: "View Car Insurance"}
+            }
+            ,
+            {
+                name: "category",
+                isMatch: () => (/\/home-insurance$/).test(window.location.pathname),
+                interaction: {name: "View Home Insurance"}
+            }
+            ,
+            {
+                name: "category",
+                isMatch: () => (/\/life-insurance$/).test(window.location.pathname),
+                interaction: {name: "View Life Insurance"}
+            }
+            
         ] 
     };
 
